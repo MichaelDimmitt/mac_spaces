1f01c4bb0715d7935c1df504eaf5226a30a1b62b
Michael Dimmitt <michaeldimmitt@Michaels-MBP.lan>
2019-09-05 14:05:33 -0400

    fail condition and pass condition.

---
+++ spaces.sh
@@ -0,0 +1,7 @@
+spaces(){
+  echo "$*"
+  # incorrect output: open /Users/michaeldimmitt/Desktop/config/The Matrix - Clubbed to Death [10 hours]-lSlk_KG0p44.webm
+  # goal output: open /Users/michaeldimmitt/Desktop/config/The Matrix - Clubbed to Death [10 hours]-lSlk_KG0p44.webm
+}
+spaces open /Users/michaeldimmitt/Desktop/config/The\ Matrix\ -\ Clubbed\ to\ Death\ \[10\ hours\]-lSlk_KG0p44.webm
+spaces open /Users/michaeldimmitt/Desktop/config/The Matrix - Clubbed to Death [10 hours]-lSlk_KG0p44.webm
